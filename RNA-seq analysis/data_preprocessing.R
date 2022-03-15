81757015756d7e04566d6d53e4dc0e5dc83c5f2f
Douwe-m <metselaar.douwe@gmail.com>
2022-03-15 17:08:46 +0100

    restructured rna-seq analysis

---
+++ RNA-seq analysis/data_preprocessing.R
@@ -0,0 +1,25 @@
+#Filter samples available at baseline
+counts_filtered <- counts %>% 
+  select(ends_with("_1")) %>% 
+  rename_with(~gsub("_1|X", "", .x))
+
+#Select required columns
+patient_data <- patient_data %>% 
+  select(idnr, treatment, geslacht, lftv1, rookjaar, rooknuv1, packyearv1, 
+         ccqtotalv1, ccqtotalv3, rvtlcpredv1, rvtlcpredv3, fevnapredv1, fevnapredv3) 
+
+#Filter for patients that have RNA-seq available at baseline, no NA in any column and have received no placebo
+patient_data <- patient_data %>% 
+  filter(idnr %in% as.numeric(colnames(counts_filtered))) %>% 
+  filter(treatment != 1) %>% 
+  drop_na()
+
+#Add a column with the change in lung function after 6 months
+patient_data <- patient_data %>% 
+  mutate(d_fev_pred = fevnapredv3 - fevnapredv1) %>% 
+  mutate(d_ccq = ccqtotalv3 - ccqtotalv1) %>% 
+  mutate(d_rvtlc = rvtlcpredv3 - rvtlcpredv1)
+
+#Final selection of patients
+counts_filtered <- counts_filtered %>%
+  select(which(colnames(counts_filtered) %in% patient_data$idnr))
---
+++ RNA-seq analysis/main.R
@@ -0,0 +1,13 @@
+# Load required packages 
+library(tidyverse)
+library(edgeR)
+
+
+
+source("RNA-seq analysis/load_data.R")
+
+source("RNA-seq analysis/data_preprocessing.R")
+
+source("RNA-seq analysis/gene_expression_analysis.R")
+
+source("RNA-seq analysis/data_visualisation.R")
