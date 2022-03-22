e19c71a13f22c074152854954a9e184c309fbf0c
Douwe-m <metselaar.douwe@gmail.com>
2022-03-22 15:59:46 +0100

    Added voom transformation

--- Microarray analysis/20_differential_expression_analysis.R
+++ Microarray analysis/20_differential_expression_analysis.R
@@ -1,10 +1,15 @@
 #Design matrix
-mm <- model.matrix(~ patient_data$d_fev_pred)
+mm <- model.matrix(~ patient_data$d_ccq)
 row.names(mm) <- patient_data$patientID
 
-fit <- lmFit(expression_data_cleaned, mm)
+d <- voom(expression_data_cleaned, mm)
+
+fit <- lmFit(d, mm)
 
 efit <- eBayes(fit)
 
-tt <-topTable(fit)
+tt <-topTable(efit)
 tt
+
+https://rdrr.io/bioc/limma/man/dupcor.html
+https://here.r-lib.org/
--- Microarray analysis/main.R
+++ Microarray analysis/main.R
@@ -4,3 +4,4 @@ source("Microarray analysis/00_load_data.R", echo = T)
 
 #Pre-process data
 source("Microarray analysis/10_data_preprocessing.R", echo = T)
+
