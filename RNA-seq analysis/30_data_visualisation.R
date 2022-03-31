523f25d48d7486cc16d4a4fabb11122f1cec443b
Douwe-m <metselaar.douwe@gmail.com>
2022-03-31 15:37:01 +0200

    Made some small changes

--- RNA-seq analysis/30_data_visualisation.R
+++ RNA-seq analysis/30_data_visualisation.R
@@ -1,67 +1,9 @@
-#Scatter plot of expression vs change in clinical parameter
-
-if (sum(DEG_d_ccq$FDR <= 0.25) > 0) {
-
-  scatter_plot <- norm_counts %>% 
-    rename_with(~gsub("X", "", .x)) %>% 
-    rownames_to_column("geneID") %>% 
-    filter(geneID %in% DEG_d_ccq[DEG_d_ccq$FDR <=0.05, 1]) %>% 
-    pivot_longer(!geneID, names_to = "patient", values_to = "expr") %>% 
-    mutate(patient = as.numeric(patient)) %>%
-    left_join(y = patient_data %>% select(idnr, d_ccq), by = c("patient" = "idnr")) %>% 
-    
-    ggplot(aes(x = expr, y = d_ccq, group = factor(geneID))) +
-      geom_point() +
-      labs(x = "Log2 TMM values", y = expression(Delta ~ "CCQ Total Score")) +
-      facet_wrap(. ~ geneID, ncol = 2) 
-  
-  ggsave(filename = "RNA-seq analysis/results/scatter_plot_d_ccq.png",
-         plot = scatter_plot)
-}
-
-if (sum(DEG_d_fev$FDR <= 0.25) > 0) {
-  
-  scatter_plot <- norm_counts %>% 
-    rename_with(~gsub("X", "", .x)) %>% 
-    rownames_to_column("geneID") %>% 
-    filter(geneID %in% DEG_d_fev[DEG_d_fev$FDR <=0.05, 1]) %>% 
-    pivot_longer(!geneID, names_to = "patient", values_to = "expr") %>% 
-    mutate(patient = as.numeric(patient)) %>%
-    left_join(y = patient_data %>% select(idnr, d_fev_pred), by = c("patient" = "idnr")) %>% 
-    
-    ggplot(aes(x = expr, y = d_fev_pred, group = factor(geneID))) +
-      geom_point() +
-      labs(x = "Log2 TMM values", y = expression(Delta ~ "FEV1 % pred")) +
-      facet_wrap(. ~ geneID, ncol = 2) 
-  
-  ggsave(filename = "RNA-seq analysis/results/scatter_plot_fev.png",
-         plot = scatter_plot)
-}
-
-if (sum(DEG_d_rvtlc$FDR <= 0.25) > 0) {
-  
-  scatter_plot <- norm_counts %>% 
-    rename_with(~gsub("X", "", .x)) %>% 
-    rownames_to_column("geneID") %>% 
-    filter(geneID %in% DEG_d_rvtlc[DEG_d_rvtlc$FDR <=0.05, 1]) %>% 
-    pivot_longer(!geneID, names_to = "patient", values_to = "expr") %>% 
-    mutate(patient = as.numeric(patient)) %>%
-    left_join(y = patient_data %>% select(idnr, d_rvtlc), by = c("patient" = "idnr")) %>% 
-    
-    ggplot(aes(x = expr, y = d_rvtlc, group = factor(geneID))) +
-      geom_point() +
-      labs(x = "Log2 TMM values", y = expression(Delta ~ "RV/TLC % pred")) +
-      facet_wrap(. ~ geneID, ncol = 2) 
-  
-  ggsave(filename = "RNA-seq analysis/results/scatter_plot_d_rvtlc.png",
-         plot = scatter_plot)
-}
-
-#3x3 scatter plot of genes of interest (GOI)
-GOI <- c("ENSG00000133110", "ENSG00000161905", "ENSG00000264940")
+#Correlate identified genes to change in clinical parameters
+GOI <- DEG_d_ccq %>% 
+  filter(FDR <= 0.10) %>% 
+  pull(ensembl_gene_id)
 
 temp_data <- norm_counts %>% 
-  rename_with(~gsub("X", "", .x)) %>% 
   rownames_to_column("geneID") %>% 
   filter(geneID %in% GOI) %>% 
   pivot_longer(!geneID, names_to = "patient", values_to = "expr") %>% 
@@ -69,15 +11,14 @@ temp_data <- norm_counts %>%
   left_join(y = patient_data %>% select(idnr, d_ccq, d_fev_pred, d_rvtlc), by = c("patient" = "idnr")) %>% 
   pivot_longer(cols = c(d_ccq, d_fev_pred, d_rvtlc), names_to = "clin_param", values_to = "values")
 
-plot <- temp_data %>%   
+temp_data %>%   
   ggplot(aes(x = expr, y = values)) +
-    geom_point() +
-    geom_smooth(method = lm, se = F)+
-    facet_wrap(geneID ~ clin_param, scales = "free") +
-    geom_label(aes(label = patient))
-  
-ggsave(filename = "RNA-seq analysis/results/scatter_plot_3_genes.png",
-       plot = plot)
+  geom_point() +
+  geom_smooth(method = lm, se = F)+
+  facet_wrap(clin_param ~ geneID, scales = "free", ncol = 6)
+
+# ggsave(filename = "RNA-seq analysis/results/scatter_plot_3_genes.png",
+#        plot = plot)
 
 #Calculate correlations
 temp_data %>% 
@@ -88,9 +29,17 @@ temp_data %>%
 
 
 
+temp_data %>% 
+  select(geneID, patient, expr) %>% 
+  distinct() %>% 
+  ggplot(aes(x = geneID, y = expr)) +
+  geom_boxplot() +
+  geom_jitter()
+   
 
-
-
-
-
-
+temp_data %>% 
+  select(geneID, patient, expr) %>% 
+  distinct() %>% 
+  ggplot(aes(x = geneID, y = expr)) +
+  geom_boxplot() +
+  geom_jitter()
--- RNA-seq analysis/main.R
+++ RNA-seq analysis/main.R
@@ -9,7 +9,7 @@ source("RNA-seq analysis/10_data_preprocessing.R", echo = T)
 source("RNA-seq analysis/20_gene_expression_analysis.R", echo = T)
 
 
-#-------------Visualize RNA-seq results-------------
+#-------------Identified DEGs with change in clin. para. after 6 months -------------
 #read data files
 source("RNA-seq analysis/00_load_data.R", echo = T)
 
@@ -19,28 +19,32 @@ source("RNA-seq analysis/10_data_preprocessing.R", echo = T)
 #Visualize the results
 source("RNA-seq analysis/30_data_visualisation.R", echo = T)
 
+#GSVA analysis
+source("RNA-seq analysis/40_GSVA_analysis.R", echo = T) #Why not compare signature with all patients with or without counts?
 
-#-------------GSVA analysis-------------
+
+#-------------Identified DEGs with change in clin. para. after 30 months -------------
 #read data files
 source("RNA-seq analysis/00_load_data.R", echo = T)
 
 #Pre-process data
-source("RNA-seq analysis/10_data_preprocessing.R", echo = T)
+source("RNA-seq analysis/11_v11_data_preprocessing.R", echo = T)
+
+#Visualize the results
+source("RNA-seq analysis/30_data_visualisation.R", echo = T)
 
 #GSVA analysis
 source("RNA-seq analysis/40_GSVA_analysis.R", echo = T)
 
 
+
+#-------------Other analyses-------------
 #-------------DGE analysis over 30 months-------------
 #read data files
 source("RNA-seq analysis/00_load_data.R", echo = T)
 
 #Pre-process data and DGE analysis
 source("RNA-seq analysis/21_v11_vs_baseline.R")
-
-for (i in res_v11){
-  print(i[1:5,])
-}
 
 #-------------DGE analysis with outliers removed-------------
 #Patients 2109 and 2107 were removed since they are possible outliers 
@@ -52,39 +56,3 @@ source("RNA-seq analysis/10_data_preprocessing.R", echo = T)
 
 #Perform analysis again, but with outliers (2109 and 2107) removed
 source("RNA-seq analysis/22_gene_expression_analysis_patients_removed.R", echo = T)
-
-for (i in top_genes) {
-  print(i[1:5,])
-}
-
-#-------------DGEs with FDR <= 0.10 compared to clin. para. 6 months-------------
-#read data files
-source("RNA-seq analysis/00_load_data.R", echo = T)
-
-#Pre-process data
-source("RNA-seq analysis/10_data_preprocessing.R", echo = T)
-
-DEG_d_ccq %>% 
-  filter(FDR <= 0.10) %>% 
-  pull(ensembl_gene_id)
-
-DEG_d_fev %>% 
-  filter(FDR <= 0.10) %>% 
-  pull(ensembl_gene_id)
-
-DEG_d_rvtlc %>% 
-  filter(FDR <= 0.10) %>% 
-  pull(ensembl_gene_id)
-
-#GSVA analysis with change in clinical parameters after 6 months
-source("RNA-seq analysis/40_GSVA_analysis.R", echo = T)
-
-#-------------DGEs with FDR <= 0.10 compared to clin. para. 30 months-------------
-#read data files
-source("RNA-seq analysis/00_load_data.R", echo = T)
-
-#Pre-process data
-source("RNA-seq analysis/11_v11_data_preprocessing.R", echo = T)
-
-#GSVA analysis with change in clinical parameters after 30 months
-source("RNA-seq analysis/40_GSVA_analysis.R", echo = T)
