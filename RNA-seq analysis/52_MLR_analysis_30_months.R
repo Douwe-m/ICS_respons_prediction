024dcbe76f6dd4171f3694a6baf7627e9c1642da
Douwe-m <metselaar.douwe@gmail.com>
2022-04-19 17:14:54 +0200

    Script to perform MLR with change after 30 months

---
+++ RNA-seq analysis/52_MLR_analysis_30_months.R
@@ -0,0 +1,41 @@
+#prepare enrichment scores
+es <- gsva_es %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  rownames_to_column("patient") %>% 
+  mutate(patient = as.numeric(patient))
+
+#Get CPM norm counts
+GOI <- DEG_d_ccq %>% 
+  filter(FDR <= 0.10) %>% 
+  pull(ensembl_gene_id)
+
+cpm_counts <- cpm(counts_filtered, log = T) %>% 
+  as.data.frame() %>% 
+  rownames_to_column("geneID") %>% 
+  filter(geneID %in% GOI) %>% 
+  pivot_longer(!geneID, names_to = "patient", values_to = "expr") %>% 
+  pivot_wider(names_from = geneID, values_from = expr) %>% 
+  mutate(patient = as.numeric(patient))
+
+#clean patient data
+patient_data_subset <- patient_data_raw %>% 
+  filter(treatment != 1) %>% 
+  left_join(es, by = c("idnr" = "patient")) %>% 
+  select(idnr, treatment, geslacht, lftv1, rooknuv1, packyearv1, lengtev1, gewichtv1,   
+         ccqtotalv1, ccqtotalv11, rvtlcpredv1, rvtlcpredv11, fevnapredv1, fevnapredv11,
+         fvcnapredv1, fevfvcnapredv1, tlcv1,
+         eospv1, eosv1, speov1, cyspteov1, 
+         neutropv1, spneutrov1, cysptneuv1,
+         lymfopv1, lymfov1, splymv1, cysptlymv1,
+         monopv1, monov1, 
+         signature = d_ccq_sig) %>% 
+  mutate(d_fev_pred = fevnapredv11 - fevnapredv1,
+         d_ccq = ccqtotalv11 - ccqtotalv1,
+         d_rvtlc = rvtlcpredv11 - rvtlcpredv1) %>% 
+  left_join(cpm_counts, by = c("idnr" = "patient"))
+
+#Remove variables not to include
+vars <- patient_data_subset %>% 
+  drop_na() %>% 
+  select(-treatment, -geslacht, -rooknuv1, -ccqtotalv11, -rvtlcpredv11, -fevnapredv11)
