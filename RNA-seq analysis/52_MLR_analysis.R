478c8f7ed20cd12d8d839378a39215cbd659f8df
Douwe-m <metselaar.douwe@gmail.com>
2022-04-19 17:14:25 +0200

    Added a column with FDR values

--- RNA-seq analysis/52_MLR_analysis.R
+++ RNA-seq analysis/52_MLR_analysis.R
@@ -28,8 +28,11 @@ vars %>%
 
 #Calculate correlations
 cor_matrix <-  corr.test(vars %>% select(-idnr), method = "spearman", adjust = "none")
+cor_matrix_padj <-  corr.test(vars %>% select(-idnr), method = "spearman", adjust = "fdr")
+
 correlations_r <- cor_matrix$r %>% as.data.frame() %>% select(starts_with("d_")) %>% rownames_to_column("parameter")
 correlations_p <- cor_matrix$p %>% as.data.frame() %>% select(starts_with("d_")) %>% rownames_to_column("parameter")
+correlations_padj <- cor_matrix_padj$p %>% as.data.frame() %>% select(starts_with("d_")) %>% rownames_to_column("parameter")
 
 #Results from correlation analysis
 d_fev_cor <- correlations_r %>% 
@@ -37,7 +40,9 @@ d_fev_cor <- correlations_r %>%
   filter(!grepl('d_', parameter)) %>% 
   left_join(correlations_p %>% select(parameter, d_fev_pred),
             by = "parameter") %>% 
-  rename("r" = d_fev_pred.x, "p" = d_fev_pred.y) %>% 
+  left_join(correlations_padj %>% select(parameter, d_fev_pred),
+            by = "parameter") %>% 
+  rename("r" = d_fev_pred.x, "p" = d_fev_pred.y, "FDR" = d_fev_pred) %>% 
   arrange(p)
 
 d_ccq_cor <- correlations_r %>% 
@@ -45,7 +50,9 @@ d_ccq_cor <- correlations_r %>%
   filter(!grepl('d_', parameter)) %>% 
   left_join(correlations_p %>% select(parameter, d_ccq),
             by = "parameter") %>% 
-  rename("r" = d_ccq.x, "p" = d_ccq.y) %>% 
+  left_join(correlations_padj %>% select(parameter, d_ccq),
+            by = "parameter") %>% 
+  rename("r" = d_ccq.x, "p" = d_ccq.y, "FDR" = d_ccq) %>% 
   arrange(p)
 
 d_rvtlc_cor <- correlations_r %>% 
@@ -53,7 +60,9 @@ d_rvtlc_cor <- correlations_r %>%
   filter(!grepl('d_', parameter)) %>% 
   left_join(correlations_p %>% select(parameter, d_rvtlc),
             by = "parameter") %>% 
-  rename("r" = d_rvtlc.x, "p" = d_rvtlc.y) %>% 
+  left_join(correlations_padj %>% select(parameter, d_rvtlc),
+            by = "parameter") %>% 
+  rename("r" = d_rvtlc.x, "p" = d_rvtlc.y, "FDR" = d_rvtlc) %>% 
   arrange(p) %>% 
   format(scientific=FALSE)
 
@@ -61,11 +70,29 @@ d_fev_cor
 d_ccq_cor
 d_rvtlc_cor
 
-#multiple linear regression
+#multiple linear regression p<0.10
+fit_d_ccq <- lm(formula = d_ccq ~ lftv1 + lymfopv1, data = patient_data_subset)
+fit_d_fev <- lm(formula = d_fev_pred ~ lengtev1, data = patient_data_subset)
+fit_d_rvtlc <- lm(formula = d_rvtlc ~ rvtlcpredv1 + fevfvcnapredv1 + ccqtotalv1 + monopv1 + lftv1 + fvcnapredv1 + 
+                    eosv1, data = patient_data_subset)
+
+#multiple linear regression p<0.20
 fit_d_ccq <- lm(formula = d_ccq ~ lftv1 + lymfopv1 + ccqtotalv1 + signature + cysptlymv1 + neutropv1, data = patient_data_subset)
 fit_d_fev <- lm(formula = d_fev_pred ~ lengtev1 + tlcv1 + cysptneuv1 + monov1, data = patient_data_subset)
 fit_d_rvtlc <- lm(formula = d_rvtlc ~ rvtlcpredv1 + fevfvcnapredv1 + ccqtotalv1 + monopv1 + lftv1 + fvcnapredv1 + 
                     eosv1 + eospv1 + packyearv1 + spneutrov1 + cysptneuv1, data = patient_data_subset)
+
+#multiple linear regression p<0.10 with genes
+fit_d_ccq <- lm(formula = d_ccq ~ ENSG00000161905 + lftv1 + lymfopv1, data = vars)
+fit_d_fev <- lm(formula = d_fev_pred ~ lengtev1 + ENSG00000172752, data = vars)
+fit_d_rvtlc <- lm(formula = d_rvtlc ~ rvtlcpredv1 + fevfvcnapredv1 + ccqtotalv1 + monopv1 + lftv1 + fvcnapredv1 + 
+                    eosv1, data = vars)
+
+#multiple linear regression p<0.20 with genes
+fit_d_ccq <- lm(formula = d_ccq ~ ENSG00000161905 + lftv1 + lymfopv1 + ccqtotalv1 + signature + cysptlymv1 + neutropv1, data = vars)
+fit_d_fev <- lm(formula = d_fev_pred ~ lengtev1 + ENSG00000172752 + tlcv1 + cysptneuv1 + monov1, data = vars)
+fit_d_rvtlc <- lm(formula = d_rvtlc ~ rvtlcpredv1 + fevfvcnapredv1 + ccqtotalv1 + monopv1 + lftv1 + fvcnapredv1 + 
+                    eosv1 + eospv1 + packyearv1 + spneutrov1 + cysptneuv1, data = vars)
 
 summary(fit_d_ccq)
 summary(fit_d_fev)
