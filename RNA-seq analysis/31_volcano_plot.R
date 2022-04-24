c293fc2f92d91e04bc2b5e71d6348b85097c8097
Douwe-m <metselaar.douwe@gmail.com>
2022-04-24 13:47:15 +0200

    Draw a correct volcano plot

--- RNA-seq analysis/31_volcano_plot.R
+++ RNA-seq analysis/31_volcano_plot.R
@@ -1,5 +1,6 @@
 library(EnhancedVolcano)
 library(ggrepel)
+library("ggtext")
 
 my_theme<-theme_classic()+
   theme(panel.border = element_rect(color='black', fill=NA, size=1.2),
@@ -22,12 +23,30 @@ new_DEGs <- DEG_d_ccq %>%
   mutate(gene_symbol = ifelse(is.na(temp_symbols), ensembl_gene_id, temp_symbols)) %>% 
   select(-hgnc_symbol, -temp_symbols)
 
+y.bar.intercept <- new_DEGs %>%
+  dplyr::filter(FDR >=0.10) %>% 
+  dplyr::pull(PValue) %>%
+  min() %>%
+  log10() %>%
+  (function(x) {x * -1})
 
-ggplot(new_DEGs, aes(x = logFC, y = -log10(FDR))) +
-  geom_hline(aes(yintercept = -log10(0.10)), linetype = "dashed") +
+
+
+ggplot(new_DEGs, aes(x = logFC, y = -log10(PValue))) +
+  geom_hline(aes(yintercept = y.bar.intercept), linetype = "dashed") +
   geom_point(alpha = 0.25) +
   geom_point(data = subset(new_DEGs, FDR<=0.10), color = "lightcoral", size = 3) +
-  geom_text_repel(data = subset(new_DEGs, FDR<=0.10), aes(x = logFC, y = -log10(FDR), label = gene_symbol)) +
+  geom_text_repel(data = subset(new_DEGs, FDR<=0.10), aes(x = logFC, y = -log10(PValue), label = gene_symbol)) +
   scale_x_continuous(breaks = seq(-3, 3, 1), limits = c(-3, 3)) +
-  scale_y_continuous(breaks = seq(0, 3, 1), limits = c(0, 3)) +
-  my_theme
+  scale_y_continuous(breaks = seq(0, 8, 1), limits = c(0, 8)) +
+  my_theme +
+  theme(axis.title.y = element_markdown(),
+        axis.title.x = element_markdown()) +
+  labs(x = "log<sub>2</sub>(FC)", 
+       y = "-log<sub>10</sub>(p)")
+
+
+
+
+
+
